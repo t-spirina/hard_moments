94911da42810611ee9ac314cd6eb438f1e0a3618
[Tanya] <[spirina.tasya@yandex.ru]>
2021-12-14 21:25:10 +0300

    lesson6

--- main.cpp
+++ main.cpp
@@ -1,55 +1,137 @@
 #include <iostream>
-#include <deque>
-#include <list>
-#include <numeric>
-#include "insert_sorted.h"
-#include "RMSerror.h"
-namespace conteiner {
-template <class CONTEINER>
-void cout(CONTEINER& forOutput)
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <set>
+#include <future>
+#include <chrono>
+#include <queue>
+
+/*
+Создайте потокобезопасную оболочку для объекта cout. Назовите ее pcout.
+Необходимо, чтобы несколько потоков могли обращаться к pcout и информация выводилась в консоль.
+Продемонстрируйте работу pcout.
+*/
+std::mutex pcoutMutex;
+template <typename T>
+void pcout(T dataForPrint)
 {
-    for(auto element:forOutput)
-        std::cout<<element<<" ";
-    std::cout<<std::endl;
+       std::lock_guard<std::mutex> lock{pcoutMutex};
+       std::cout << dataForPrint;
 }
+
+
+/*
+Реализовать функцию, возвращающую i-ое простое число (например, миллионное простое число равно 15485863).
+Вычисления реализовать во вторичном потоке. В консоли отображать прогресс вычисления. */
+std::mutex simpleMutex;
+bool isSimple(int num)
+{
+    if (num==2) return true;
+    if ((num<2)||(num%2==0)) return false;
+    for(int i=3;i<num/2;i=i+2)
+        if (num%i==0) return false;
+    return true;
 }
+
+void getSimpleNum (int Smpl_i, int &count, int &simpleNum)
+{
+    for (simpleNum=0; count<Smpl_i;simpleNum++)
+    {
+        if (isSimple(simpleNum))
+        {
+            simpleMutex.lock();
+            count++;
+            if (count==Smpl_i)
+            {
+                simpleMutex.unlock();
+                break;
+            }
+            simpleMutex.unlock();
+        }
+    }
+    return;
+}
+/*
+Промоделировать следующую ситуацию. Есть два человека (2 потока): хозяин и вор.
+Хозяин приносит домой вещи (функция добавляющая случайное число в вектор с периодичностью 1 раз в секунду).
+При этом у каждой вещи есть своя ценность. Вор забирает вещи (функция, которая находит наибольшее число и удаляет из вектора
+с периодичностью 1 раз в 0.5 секунд), каждый раз забирает вещь с наибольшей ценностью.*/
+using namespace std::chrono_literals;
+std::mutex  lock_boxes;
+struct Box
+{
+    int16_t m_id;
+    int16_t price;
+    Box(int16_t box_num, int16_t box_price): m_id(box_num), price(box_price) {}
+};
+
+bool operator< (const Box &lbox, const Box &rbox)
+{
+    return lbox.price < rbox.price;
+}
+
+//std::priority_queue<Box> shelf;
+std::multiset <Box> shelf;
+const int numberOfBoxes = 10;
+bool end;
+
+void Human()
+{
+    for (int i = 0; i < numberOfBoxes; ++i)
+    {
+        std::this_thread::sleep_for(1000ms);
+        {
+            std::lock_guard shelf_lock_guard(lock_boxes);
+            const std::pair<size_t, int16_t> box {i + 1, rand()% 300};
+            shelf.insert(Box(box.first, box.second));
+            std::cout << "new box on the shelf:   " << box.first << " price " << box.second << std::endl;
+        }
+        std::lock_guard shelf_lock_guard(lock_boxes);
+        end = true;
+    }
+}
+
+void Thief()
+{
+    for (int i = 0; i < numberOfBoxes; ++i)
+    {
+        std::this_thread::sleep_for(1500ms);
+        {
+            std::lock_guard shelf_lock_guard(lock_boxes);
+            std::cout << "Stollen box " << shelf.end()->m_id << std::endl;
+
+            shelf.erase(--shelf.end());
+
+        }
+    }
+}
+
+
+
 
 int main()
 {
-    /*
-    1. имеется отсвортированный массив целых чисел. Необходимо разработать функцию insert_sorted,
-    которая принимает вектор и новое число и вставляет новое число в определенную позицию в векторе, чтобы упорядоченность контейнера сохранялась.
-    реализуйте шаблонную функцию insert_sorted, которая сможет аналогично работать с любым контейнером, содержащим любой тип значений
-    */
-    std::vector<int> vect1 ={1,2,4,7,9,0};
-    int n=5;
-    std::cout<<"vector1:"<<std::endl;
-    std::sort(vect1.rbegin(),vect1.rend());
-    conteiner::cout(vect1);
-    insert_sorted_vect(vect1,n);
-    conteiner::cout(vect1);
 
-    std::cout<<"vector:"<<std::endl;
-    insert_sorted(vect1,n);
-    conteiner::cout(vect1);
+   int count=0;
+   int num=10000;
+   int simple=0;
+   std::thread countSimple(getSimpleNum, num, std::ref(count), std::ref(simple));
+   countSimple.detach();
+   while(count!=num)
+   {
+       std::this_thread::sleep_for(50ms);
+       simpleMutex.lock();
+           std::cout<<count*100/num<<" \n";
+       simpleMutex.unlock();
+   }
+   std::cout<<simple<<std::endl;
 
-    std::cout<<"list:"<<std::endl;
-    std::list <int> list={3,5,7};
-    insert_sorted(list,n);
-    conteiner::cout(list);
+   std::thread human (Human);
+   std::thread thief (Thief);
+      human.join();
+      thief.join();
 
-    std::cout<<"deque:"<<std::endl;
-    std::deque <int> deque={7,3,5,2,0};
-    std::sort(deque.begin(),deque.end());
-    insert_sorted(deque,n);
-    conteiner::cout(deque);
 
-    std::vector<float> analogSignal(100);
-    std::generate(analogSignal.begin(), analogSignal.end(), [] () mutable { return (std::rand()%100)/3.5; });
-    conteiner::cout(analogSignal);
-
-    std::vector<int> digitalSignal(analogSignal.begin(), analogSignal.end());
-    conteiner::cout(digitalSignal);
-    std::cout<<"RMS error: "<<RMSerror(analogSignal,digitalSignal)<<std::endl;
-    return 0;
+   return 0;
 }
